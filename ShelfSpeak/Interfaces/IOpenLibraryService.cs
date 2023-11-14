c1015324ce8a7dbbc2ff3350f9718dd82de7da49
abukhmirov <alexbukhmirov@gmail.com>
2023-11-14 09:49:37 -0700

    API data shows up!

--- ShelfSpeak/Interfaces/IOpenLibraryService.cs
+++ ShelfSpeak/Interfaces/IOpenLibraryService.cs
@@ -1,6 +1,9 @@
-﻿namespace ShelfSpeak.Interfaces
+﻿using ShelfSpeak.Models.APIJsonShenanigans;
+
+namespace ShelfSpeak.Interfaces
 {
     public interface IOpenLibraryService
     {
+        Docs SearchBooksOLIDJson(string query);
     }
 }
---
+++ ShelfSpeak/Models/APIJsonShenanigans/OpenLibraryAPIResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace ShelfSpeak.Models.APIJsonShenanigans
+{
+    public class OpenLibraryAPIResponse
+    {
+        public int numFound { get; set; }
+        public int start { get; set; }
+        public bool numFoundExact { get; set; }
+        public List<Docs> Docs { get; set; }
+    }
+}
