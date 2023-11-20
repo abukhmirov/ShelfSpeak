e6179c498757974e3a42abb74c774890756f1c2f
abukhmirov <alexbukhmirov@gmail.com>
2023-11-20 00:21:50 -0700

    MessagesController started

---
+++ ShelfSpeak/Controllers/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShelfSpeak.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
